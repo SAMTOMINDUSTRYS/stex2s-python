cae2aba2ce15a270d6e9c759cb55de2b8f71c923
Sam Nicholls <samstudio8@gmail.com>
2021-07-25 13:28:44 +0100

    Get some organisation up in here

--- stexs-py/stexs/domain/model.py
+++ stexs-py/stexs/domain/model.py
@@ -3,9 +3,9 @@ from typing import List, Dict
 import logging
 import time
 import copy
-from rich.logging import RichHandler
 
 # https://rich.readthedocs.io/en/stable/logging.html
+from rich.logging import RichHandler
 log = logging.getLogger("rich")
 FORMAT = "%(message)s"
 logging.basicConfig(
@@ -473,95 +473,3 @@ class Exchange:
 
         # Idempotent txid
         self.txid_set.add(msg["txid"])
-
-if __name__ == "__main__":
-    stex = Exchange()
-    stocks = [
-        Stock(symbol="STI.", name="Sam and Tom Industrys"),
-        Stock(symbol="ARRM", name="AbeRystwyth RISC Machines"),
-        Stock(symbol="ELAN", name="Elan Dataworks"),
-    ]
-    stex.add_stocks(stocks)
-
-    clients = [
-        Client(csid="1", name="Sam"),
-    ]
-    stex.add_users(clients)
-    stex.adjust_balance(csid="1", adjust_balance=+100000)
-    stex.adjust_holding(csid="1", symbol="STI.", adjust_qty=+10000)
-    stex.adjust_holding(csid="1", symbol="ELAN", adjust_qty=+10000)
-
-    stex.recv({
-        "txid": "1",
-        "csid": "1",
-        "side": "SELL",
-        "symbol": "STI.",
-        "price": 8.02,
-        "volume": 1000,
-    })
-    stex.recv({
-        "txid": "2",
-        "csid": "1",
-        "side": "SELL",
-        "symbol": "STI.",
-        "price": 8.02,
-        "volume": 1000,
-    })
-    stex.recv({
-        "txid": "3",
-        "csid": "1",
-        "side": "BUY",
-        "symbol": "STI.",
-        "price": 8.02,
-        "volume": 500,
-    })
-    stex.recv({
-        "txid": "4",
-        "csid": "1",
-        "side": "BUY",
-        "symbol": "STI.",
-        "price": 8.02,
-        "volume": 500,
-    })
-    stex.recv({
-        "txid": "5",
-        "csid": "1",
-        "side": "SELL",
-        "symbol": "STI.",
-        "price": 7.99,
-        "volume": 1000,
-    })
-    stex.recv({
-        "txid": "6",
-        "csid": "1",
-        "side": "BUY",
-        "symbol": "STI.",
-        "price": 8.03,
-        "volume": 1500,
-    })
-    stex.recv({
-        "txid": "7",
-        "csid": "1",
-        "side": "BUY",
-        "symbol": "STI.",
-        "price": 8.00,
-        "volume": 500,
-    })
-    stex.recv({
-        "txid": "8",
-        "csid": "1",
-        "side": "SELL",
-        "symbol": "ELAN",
-        "price": 4.10,
-        "volume": 1000,
-    })
-    stex.recv({
-        "txid": "9",
-        "csid": "1",
-        "side": "BUY",
-        "symbol": "ELAN",
-        "price": 4.10,
-        "volume": 500,
-    })
-
-    #stex.request_stall("STI.")
---
+++ stexs-py/stexs/entrypoints/main.py
@@ -0,0 +1,94 @@
+from stexs.domain import model
+
+
+if __name__ == "__main__":
+    stex = model.Exchange()
+    stocks = [
+        model.Stock(symbol="STI.", name="Sam and Tom Industrys"),
+        model.Stock(symbol="ARRM", name="AbeRystwyth RISC Machines"),
+        model.Stock(symbol="ELAN", name="Elan Dataworks"),
+    ]
+    stex.add_stocks(stocks)
+
+    clients = [
+        model.Client(csid="1", name="Sam"),
+    ]
+    stex.add_users(clients)
+    stex.adjust_balance(csid="1", adjust_balance=+100000)
+    stex.adjust_holding(csid="1", symbol="STI.", adjust_qty=+10000)
+    stex.adjust_holding(csid="1", symbol="ELAN", adjust_qty=+10000)
+
+    stex.recv({
+        "txid": "1",
+        "csid": "1",
+        "side": "SELL",
+        "symbol": "STI.",
+        "price": 8.02,
+        "volume": 1000,
+    })
+    stex.recv({
+        "txid": "2",
+        "csid": "1",
+        "side": "SELL",
+        "symbol": "STI.",
+        "price": 8.02,
+        "volume": 1000,
+    })
+    stex.recv({
+        "txid": "3",
+        "csid": "1",
+        "side": "BUY",
+        "symbol": "STI.",
+        "price": 8.02,
+        "volume": 500,
+    })
+    stex.recv({
+        "txid": "4",
+        "csid": "1",
+        "side": "BUY",
+        "symbol": "STI.",
+        "price": 8.02,
+        "volume": 500,
+    })
+    stex.recv({
+        "txid": "5",
+        "csid": "1",
+        "side": "SELL",
+        "symbol": "STI.",
+        "price": 7.99,
+        "volume": 1000,
+    })
+    stex.recv({
+        "txid": "6",
+        "csid": "1",
+        "side": "BUY",
+        "symbol": "STI.",
+        "price": 8.03,
+        "volume": 1500,
+    })
+    stex.recv({
+        "txid": "7",
+        "csid": "1",
+        "side": "BUY",
+        "symbol": "STI.",
+        "price": 8.00,
+        "volume": 500,
+    })
+    stex.recv({
+        "txid": "8",
+        "csid": "1",
+        "side": "SELL",
+        "symbol": "ELAN",
+        "price": 4.10,
+        "volume": 1000,
+    })
+    stex.recv({
+        "txid": "9",
+        "csid": "1",
+        "side": "BUY",
+        "symbol": "ELAN",
+        "price": 4.10,
+        "volume": 500,
+    })
+
+    #stex.request_stall("STI.")
