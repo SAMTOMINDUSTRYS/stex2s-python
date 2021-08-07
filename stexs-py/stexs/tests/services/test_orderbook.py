254912646c7234071d62551cde15497a7e6ebd75
Sam Nicholls <samstudio8@gmail.com>
2021-08-07 13:55:33 +0100

    Split tests dir to domain and service tests

--- stexs-py/stexs/tests/services/test_orderbook.py
+++ stexs-py/stexs/tests/services/test_orderbook.py
@@ -219,6 +219,7 @@ def test_match_orderbook():
     assert expected_trade == actual_trade
 
 
+# test_order tests the function of split_sell, here we just want to check the orderbook wrapper works
 def test_split_sell():
     order = Order(txid="1", csid="1", side="SELL", symbol="STI.", price=1.0, volume=100, ts=1)
     order, remainder = orderbook.split_sell(order, 25)
@@ -229,39 +230,6 @@ def test_split_sell():
     assert remainder.txid == "1/1"
     assert remainder.volume == 25
     assert not remainder.closed
-
-
-def test_split_split_sell():
-    order = Order(txid="1/1", csid="1", side="SELL", symbol="STI.", price=1.0, volume=100, ts=1)
-    order, remainder = orderbook.split_sell(order, 25)
-
-    assert order.txid == "1/1"
-    assert order.volume == 75
-
-    assert remainder.txid == "1/2"
-    assert remainder.volume == 25
-    assert not remainder.closed
-
-
-def test_bad_split_big_volume():
-    for bad_vol in [100, 101, 1000]:
-        order = Order(txid="1/1", csid="1", side="SELL", symbol="STI.", price=1.0, volume=100, ts=1)
-
-        with pytest.raises(SplitOrderVolumeException, match="Cannot split sell for same or greater volume."):
-            order, remainder = orderbook.split_sell(order, bad_vol)
-
-def test_bad_split_small_volume():
-    for bad_vol in [0, -1, -100]:
-        order = Order(txid="1/1", csid="1", side="SELL", symbol="STI.", price=1.0, volume=100, ts=1)
-
-        with pytest.raises(SplitOrderVolumeException, match="Cannot split sell without excess volume."):
-            order, remainder = orderbook.split_sell(order, bad_vol)
-
-def test_bad_split_buy():
-    order = Order(txid="1", csid="1", side="BUY", symbol="STI.", price=1.0, volume=100, ts=1)
-
-    with pytest.raises(SplitOrderBuyException, match="Cannot split non-sell."):
-        order, remainder = orderbook.split_sell(order, 50)
 
 
 def test_execute_trade():
