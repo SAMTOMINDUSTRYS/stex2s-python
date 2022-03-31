ea0de701a66eec0b3ffeb85b7faceafa72f4b07a
Sam Nicholls <sam@samnicholls.net>
2022-03-31 16:57:38 +0100

    use env for conf for now

--- stexs-py/stexs/config.py
+++ stexs-py/stexs/config.py
@@ -1,8 +1,13 @@
+import os
+
 def get_sqlite_url():
     SQLITE_URL = "sqlite:///:memory:"
     #SQLITE_URL = "sqlite:///./test_sql.db"
     return SQLITE_URL
 
 def get_socket_host_and_port():
-    return ("localhost", 5412)
+    return (
+        os.getenv("STEX_EXCHANGE_HOST"),
+        int(os.getenv("STEX_EXCHANGE_PORT"))
+    )
 
